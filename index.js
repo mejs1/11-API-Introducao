d41c72367977dc3e5a58b562a3da40b07ecf2b2a
Maria Eduarda Jardim Souza <132668414+mejs1@users.noreply.github.com>
2023-08-04 11:53:30 +0000

    ver 36

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-  import express from "express";
-  const app = express();
-  const port = 3000;
+import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
 
-  app.get("/", (req, res) => {
-    res.json({
-      nome: "Maria Eduarda Jardim",
-    });
-    console.log("Rota / solicitada");
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    nome: "Maria Eduarda Jardim",
   });
+  console.log("Rota / solicitada");
+});
 
-  app.listen(port, () => {
-    console.log(`Serviço escutando na porta:  ${port}`);
-  });
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
 
-  import dotenv from "dotenv";
-
-  dotenv.config();
-  import { selectUsuarios } from "./bd.js";
-  app.get("/usuarios", async (req, res) => {
-    console.log("Rota GET/usuarios solicitada");
-    try {
-      const usuarios = await selectUsuarios();
-      res.json(usuarios);
-    } catch (error) {
-      res.status(error.status || 500).json({ message: error.message || "Erro!" });
-    }
-  });
+app.listen(port, () => {
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
