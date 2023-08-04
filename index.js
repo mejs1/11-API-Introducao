e3751a2865cc9057f7a9b02ee573acdd8e2f0aa8
Maria Eduarda Jardim Souza <132668414+mejs1@users.noreply.github.com>
2023-08-04 11:01:45 +0000

    ver 3

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-import express from "express";     
-const app = express();              
-const port = 3000;                  
+  import express from "express";
+  const app = express();
+  const port = 3000;
 
-app.get("/", (req, res) => {        
-  res.json({
-    nome: "Maria Eduarda Jardim",      
+  app.get("/", (req, res) => {
+    res.json({
+      nome: "Maria Eduarda Jardim",
+    });
+    console.log("Rota / solicitada");
   });
-  console.log("Rota / solicitada");
-});
 
-app.listen(port, () => {            
-  console.log(`Serviço escutando na porta:  ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Serviço escutando na porta:  ${port}`);
+  });
+
+  import dotenv from "dotenv";
+
+  dotenv.config();
+  import { selectUsuarios } from "./bd.js";
+  app.get("/usuarios", async (req, res) => {
+    console.log("Rota GET/usuarios solicitada");
+    try {
+      const usuarios = await selectUsuarios();
+      res.json(usuarios);
+    } catch (error) {
+      res.status(error.status || 500).json({ message: error.message || "Erro!" });
+    }
+  });
