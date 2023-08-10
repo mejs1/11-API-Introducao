b5db97760fec9586bd4d971ac96917fc265cb201
Maria Eduarda Jardim Souza <132668414+mejs1@users.noreply.github.com>
2023-08-10 13:30:12 +0000

    aoaoaoao

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import { selectUsuarios } from "./bd.js";
+import { selectUsuario } from "./bd.js";
+import {  insertUsuario } from "./bd.js";
 
 dotenv.config();
 
@@ -24,6 +26,28 @@ app.get("/usuarios", async (req, res) => {
   }
 });
 
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.use(express.json());
 app.listen(port, () => {
   console.log(`Serviço escutando na porta:  ${port}`);
 });
