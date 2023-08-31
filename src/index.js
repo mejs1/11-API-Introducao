0f5321f8beebcd951c31fed2a2a9d720123293d1
Maria Eduarda Jardim Souza <132668414+mejs1@users.noreply.github.com>
2023-08-31 12:55:34 +0000

    ver 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API para CRUD usuario: link_gitgub",
+  });
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
 import roteadorUsuario from "./routes/usuario.js";
 
 app.use(roteadorUsuario);
